9620d49c4f4b0b420d75ba0ea8885023e0ad620e
Luke Roberts <lukeroberts.j+os@gmail.com>
2020-12-19 09:12:30 -0700

    First pass at incomplete set of implementations

---
+++ src/lib.rs
@@ -0,0 +1,119 @@
+#[macro_use]
+extern crate variantly_derive;
+
+pub use variantly_derive::Variantly;
+
+#[macro_export]
+macro_rules! get_or_insert {
+    ($enum:expr, $variant:path, $value:expr) => {
+        variantly::get_or_insert_with!($enum, $variant, || $value)
+    };
+}
+
+#[macro_export]
+macro_rules! get_or_insert_with {
+    ($enum:expr, $variant:path, $with:tt) => {
+        match *$enum {
+            $variant(ref mut value) => value,
+            _ => *self = $variant(with()),
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! or {
+    ($a:expr, $b:expr, $variant:path) => {
+        match (&$a, $b) {
+            (&$variant(_), $variant(val)) => $variant(val),
+            _ => $a,
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! or_else {
+    ($a:expr, $or_else:tt, $variant:path) => {
+        match $a {
+            $variant(value) => $variant($or_else(value)),
+            _ => $a,
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! unwrap_or_else {
+    ($enum:expr, $variant:path, $else:tt) => {
+        match $enum {
+            $variant(value) => value,
+            _ => $else(),
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! unwrap_or {
+    ($enum:expr, $variant:path, $or:expr) => {
+        variantly::unwrap_or_else!($enum, $variant, (|| $or))
+    };
+}
+
+#[macro_export]
+macro_rules! unwrap {
+    ($enum:expr, $variant:path) => {
+        variantly::unwrap_or_else!($enum, $variant, (|| panic!()))
+    };
+}
+
+#[macro_export]
+macro_rules! contains {
+    ($enum:expr, $variant:path, $target:expr) => {
+        match $enum {
+            $variant(value) => value == $target,
+            _ => false,
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! expect {
+    ($enum:expr, $variant:path, $msg:expr) => {
+        variantly::unwrap_or_else!($enum, $variant, (|| panic!("{}", $msg)))
+    };
+}
+
+#[macro_export]
+macro_rules! is {
+    ($enum:expr, $variant:path) => {
+        match $enum {
+            $variant(_) => true,
+            _ => false,
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! ok {
+    ($enum:expr, $variant:path) => {
+        match $enum {
+            $variant(value) => Some(value),
+            _ => None,
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! ok_or {
+    ($enum:expr, $variant:path, $err:expr) => {
+        variantly::ok_or_else!($enum, $variant, (|| $err))
+    };
+}
+
+#[macro_export]
+macro_rules! ok_or_else {
+    ($enum:expr, $variant:path, $else:tt) => {
+        match $enum {
+            $variant(value) => Ok(value),
+            _ => Err($else()),
+        }
+    };
+}
---
+++ src/variantly_derive/src/lib.rs
@@ -0,0 +1,116 @@
+extern crate proc_macro;
+
+use inflector::cases::snakecase::to_snake_case;
+use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use std::iter::repeat;
+use syn::{
+    parse_macro_input, punctuated::Punctuated, token::Comma, Field, Ident, ItemEnum, Variant,
+};
+
+#[proc_macro_derive(Variantly)]
+pub fn variantly(input: TokenStream) -> TokenStream {
+    let enm = parse_macro_input!(input as ItemEnum);
+    let enm_name = format_ident!("{}", enm.ident.to_string());
+    let enm_name = repeat(enm_name);
+
+    let variants: Vec<Variant> = enm.variants.iter().map(|variant| variant.clone()).collect();
+
+    let name: Vec<Ident> = variants
+        .iter()
+        .map(|variant| variant.ident.clone())
+        .collect();
+
+    let types: Vec<Punctuated<Field, Comma>> = variants
+        .iter()
+        .map(|variant| {
+            if let syn::Fields::Unnamed(value) = variant.fields.clone() {
+                value.unnamed
+            } else {
+                panic!();
+            }
+        })
+        .collect();
+
+    /// Helper for declaring Vec<Ident> based on snake_cased enum variant names and a given suffix.
+    macro_rules! declare_idents {
+        ($($id:ident),*) => {
+            $(
+                let name = stringify!($id);
+                let $id = variants
+                    .iter()
+                    .map(|variant| {
+                        format_ident!("{}_{}", name, to_snake_case(&variant.ident.to_string()))
+                    })
+                    .collect::<Vec<Ident>>();
+            )*
+        };
+    }
+
+    declare_idents! {is, ok, ok_or, ok_or_else, expect, contains, unwrap, unwrap_or, unwrap_or_else, is_not, or, or_else, get_or_insert_with, get_or_insert};
+
+    let result = quote! {
+        #(
+            impl #enm_name {
+
+                fn #or(self, other: #enm_name) -> #enm_name {
+                    variantly::or!(self, other, #enm_name::#name)
+                }
+
+                fn #or_else<F: FnOnce(#types) -> #types>(self, or_else: F) -> #enm_name {
+                    variantly::or_else!(self, or_else, #enm_name::#name)
+                }
+
+                fn #contains(&self, x: &#types) -> bool {
+                    variantly::contains!(self, #enm_name::#name, x)
+                }
+
+                fn #expect(self, msg: &str) -> #types {
+                    variantly::expect!(self, #enm_name::#name, msg)
+                }
+
+                fn #is(&self) -> bool {
+                    variantly::is!(self, #enm_name::#name)
+                }
+
+                fn #is_not(&self) -> bool {
+                    !self.#is()
+                }
+
+                fn #ok(self) -> Option<#types> {
+                    variantly::ok!(self, #enm_name::#name)
+                }
+
+                fn #ok_or<E>(self, or: E) -> Result<#types, E> {
+                    variantly::ok_or!(self, #enm_name::#name, or    )
+                }
+
+                fn #ok_or_else<E, F: FnOnce() -> E>(self, or_else: F) -> Result<#types, E> {
+                    variantly::ok_or_else!(self, #enm_name::#name, (or_else))
+                }
+                
+                fn #get_or_insert_with<F: FnOnce() -> #types>(&mut self, with: F) -> &mut #types {
+                    variantly::get_or_insert_with!(self, #enm_name::#name, (with))
+                }
+
+                fn #get_or_insert(&mut self, value: #types) -> &mut #types {
+                    variantly::get_or_insert!(self, #enm_name::#name, value)
+                }
+
+                fn #unwrap(self) -> #types {
+                    variantly::unwrap!(self, #enm_name::#name)
+                }
+
+                fn #unwrap_or(self, or: #types) -> #types {
+                    variantly::unwrap_or!(self, #enm_name::#name, or)
+                }
+
+                fn #unwrap_or_else(self, or_else: fn() -> #types) -> #types { // TODO should this be FnOnce
+                    variantly::unwrap_or_else!(self, #enm_name::#name, (or_else))
+                }
+            }
+        )*
+    }
+    .into();
+    result
+}
