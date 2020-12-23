46cede2c9b791540058268c2d8250c2b134a4aa3
Luke Roberts <lukeroberts.j+os@gmail.com>
2020-12-23 13:02:42 -0700

    Remove escaped var names

--- src/variantly_derive/src/derive.rs
+++ src/variantly_derive/src/derive.rs
@@ -7,13 +7,13 @@ use syn::{parse_macro_input, Fields, FieldsUnnamed, Ident, ItemEnum};
 
 pub fn derive_variantly_fns(input: TokenStream) -> TokenStream {
     // parse necessary information from input
-    let r#enum = parse_macro_input!(input as ItemEnum);
-    let enum_name = format_ident!("{}", r#enum.ident.to_string());
+    let item_enum = parse_macro_input!(input as ItemEnum);
+    let enum_name = format_ident!("{}", item_enum.ident.to_string());
 
     // For collecting impl functions
     let mut functions = vec![];
 
-    r#enum.variants.iter().for_each(|variant| {
+    item_enum.variants.iter().for_each(|variant| {
         let ident = &variant.ident;
         // This will be initialized with a tokenstream representing how to match & ignore any variables held by a variant.
         let ignore;
@@ -65,10 +65,10 @@ fn handle_unnamed(
     enum_name: &Ident,
 ) {
     // parse necessary information from fields.
-    let r#type = &fields.unnamed;
-    let vars = generate_idents(r#type.len());
+    let types = &fields.unnamed;
+    let vars = generate_idents(types.len());
     let vars = quote! { (#( #vars ),*)};
-    let r#type = quote! { #r#type };
+    let types = quote! { #types };
 
     // declare ident variables with helper macro.
     identify!(
@@ -103,7 +103,7 @@ fn handle_unnamed(
             }
         }
 
-        fn #and_then<F: FnOnce((#r#type)) -> (#r#type)>(self, and_then: F) -> #enum_name {
+        fn #and_then<F: FnOnce((#types)) -> (#types)>(self, and_then: F) -> #enum_name {
             match self {
                 #var_pattern => {
                     let #vars = and_then(#vars);
@@ -113,22 +113,22 @@ fn handle_unnamed(
             }
         }
 
-        fn #expect(self, msg: &str) -> (#r#type) {
+        fn #expect(self, msg: &str) -> (#types) {
             self.#unwrap_or_else(|| panic!("{}", msg))
         }
 
-        fn #ok(self) -> Option<(#r#type)> { // TODO: Verify this is ok for single var enums
+        fn #ok(self) -> Option<(#types)> { // TODO: Verify this is ok for single var enums
             match self {
                 #var_pattern => Some((#vars)),
                 _ => None,
             }
         }
 
-        fn #ok_or<E>(self, or: E) -> Result<(#r#type), E> {
+        fn #ok_or<E>(self, or: E) -> Result<(#types), E> {
             self.#ok_or_else(|| or)
         }
 
-        fn #ok_or_else<E, F: FnOnce() -> E>(self, or_else: F) -> Result<(#r#type), E> {
+        fn #ok_or_else<E, F: FnOnce() -> E>(self, or_else: F) -> Result<(#types), E> {
             match self {
                 #var_pattern => Ok((#vars)),
                 _ => Err(or_else())
@@ -142,7 +142,7 @@ fn handle_unnamed(
             }
         }
 
-        fn #or_else<F: FnOnce() -> (#r#type)>(self, or_else: F) -> #enum_name {
+        fn #or_else<F: FnOnce() -> (#types)>(self, or_else: F) -> #enum_name {
             match self {
                 #var_pattern => #var_pattern,
                 _ => {
@@ -152,15 +152,15 @@ fn handle_unnamed(
             }
         }
 
-        fn #unwrap(self) -> (#r#type) {
+        fn #unwrap(self) -> (#types) {
             self.#unwrap_or_else(|| panic!())
         }
 
-        fn #unwrap_or(self, or: (#r#type)) -> (#r#type) {
+        fn #unwrap_or(self, or: (#types)) -> (#types) {
             self.#unwrap_or_else(|| or)
         }
 
-        fn #unwrap_or_else<F: FnOnce() -> (#r#type)>(self, or_else: F) -> (#r#type) {
+        fn #unwrap_or_else<F: FnOnce() -> (#types)>(self, or_else: F) -> (#types) {
             match self {
                 #var_pattern => (#vars),
                 _ => or_else()
